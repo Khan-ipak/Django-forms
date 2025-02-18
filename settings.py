dd954030b67455917d73f8fbf2d0376a20563318
Your Name <azizkhanabdutolibov@gmail.com>
2025-02-18 10:59:21 +0500

    Postgres

--- settings.py
+++ settings.py
@@ -4,3 +4,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_db',       # Имя базы данных
+        'USER': 'postgres',       # Пользователь
+        'PASSWORD': 'yourpassword',  # Пароль
+        'HOST': 'localhost',      # Адрес сервера
+        'PORT': '5432',           # Порт PostgreSQL
+    }
+}
